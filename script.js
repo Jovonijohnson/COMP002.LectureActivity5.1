3f18b8a94cb5e2d9496f82471c4b57fc73e59e0c
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-13 05:26:23 -0800

    Alert button triggers only once

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Alert button event listener that triggers only once
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Button clicked!');
+}, { once: true }); // The event listener will be removed after being invoked once
+
 
