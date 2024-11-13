5342b02b045bf97fc9707e7b6685a856e4faefd5
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-13 05:26:58 -0800

    Link event listener that prevents navigation

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ document.getElementById('alertButton').addEventListener('click', function() {
     alert('Button clicked!');
 }, { once: true }); // The event listener will be removed after being invoked once
 
-
+// Link event listener that prevents navigation and shows alert
+document.getElementById('favoriteLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent navigation
+    alert('No distractions! I’m coding!');
+});
